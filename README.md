c0c7587816a9936d315235831ccea11f475151b0
auntyellow <webmaster@xqbase.com>
2015-09-10 04:00:22 +0800

    improve readme and javadoc

--- README.md
+++ README.md
@@ -4,13 +4,20 @@ Reusable Java components for [www.xqbase.com](http://www.xqbase.com/)
 ## What can I do with XQBase Util?
 XQBase Util provides simple and easy codes with following features:
 
-- [Number Parsing](#Numbers)
-- [Byte Array and InputOutput/OutputStream Operations](#Bytes)
-- [Time Operations](#Time)
-- [Configuration Files](#Conf)
-- [Logging](#Log)
-- [Pool and Singleton](#Pool)
-- []
+- [Number Parsing](#numbers)
+- [Byte Array and InputOutput/OutputStream Operations](#bytes)
+- [Time Operations](#time)
+- [Configuration Files](#conf)
+- [Logging](#log)
+- [Pool and Singleton](#pool)
+- [Windows or Linux Service](#service)
+- [Base64 (only for JDK 1.7)](#base64)
+- [Database Connections](#database-connections)
+- [HTTP Client Operations](#http-client-operations)
+- [Servlet Utils](#servlet-utils)
+- [JavaScript Server Page](#javascript-server-page)
+- [CountMap and LockMap](#countmap-and-lockmap)
+- [Functional Interfaces](#functional-interfaces)
 
 ## How do I use it?
 
@@ -37,28 +44,28 @@ or for JDK 1.7:
 ## Useful Components
 
 ### Numbers
-Help to parse numbers (int, long, float and double) and prevent unchecked [`NumberFormatException`](http://docs.oracle.com/javase/8/docs/api/java/lang/NumberFormatException.html).
+Help to parse numbers (int, long, float and double) and prevent unchecked [**NumberFormatException**](http://docs.oracle.com/javase/8/docs/api/java/lang/NumberFormatException.html).
 
-Class `Numbers` has 4 types of methods:
-- `Numbers.parseXxx(String s)` to parse a number with *default value* of `0`
-- `Numbers.parseXxx(String s, int i)` to parse a number with a given *default value* `i`
-- `Numbers.parseXxx(String s, int min, int max)` to parse a number with a range between `min` and `max` (inclusive), only for `int` so far
-- `Numbers.parseXxx(String s, int i, int min, int max)` to parse a number with a given *default value* `i` and a range between `min` and `max` (inclusive), only for `int` so far
+Class **Numbers** has 4 types of methods:
+- `Numbers.parseXxx(String s)` to parse a number with *default value* of **0**
+- `Numbers.parseXxx(String s, int i)` to parse a number with a given *default value* **i**
+- `Numbers.parseXxx(String s, int min, int max)` to parse a number with a range between **min** and **max** (inclusive), only for **int** so far
+- `Numbers.parseXxx(String s, int i, int min, int max)` to parse a number with a given *default value* **i** and a range between **min** and **max** (inclusive), only for **int** so far
 
-All these methods will return *default value* if `null` or not parsable.
+All these methods will return *default value* if null or not parsable.
 
 ### Bytes
-Various `byte[]` operations, including:
+Various **byte[]** operations, including:
 - Encoding/Decoding to/from hexadecimal
-- Storing/Retrieving `short`, `int` or `long` to/from `byte[]`
-- Concatenating/Truncating/Comparing of `byte[]`s
-- Generating random `byte[]`
+- Storing/Retrieving **short**, **int** or **long** to/from **byte[]**
+- Concatenating/Truncating/Comparing of **byte[]**s
+- Generating random **byte[]**
 - Dumping for debug
 
 ### ByteArrayQueue
-A queue of bytes like [`ByteBuffer`](http://docs.oracle.com/javase/8/docs/api/java/nio/ByteBuffer.html) but easy to add (into the tail) or retrieve (from the head).
+A queue of bytes like [**ByteBuffer**](http://docs.oracle.com/javase/8/docs/api/java/nio/ByteBuffer.html) but easy to add (into the tail) or retrieve (from the head).
 
-This queue can be used as both [`ByteArrayInputStream`](http://docs.oracle.com/javase/8/docs/api/java/io/ByteArrayInputStream.html) and [`ByteArrayOutputStream`](http://docs.oracle.com/javase/8/docs/api/java/io/ByteArrayOutputStream.html), and can be easily duplicated, e.g.
+This queue can be used as both [**ByteArrayInputStream**](http://docs.oracle.com/javase/8/docs/api/java/io/ByteArrayInputStream.html) and [**ByteArrayOutputStream**](http://docs.oracle.com/javase/8/docs/api/java/io/ByteArrayOutputStream.html), and can be easily duplicated, e.g.
 
 ```java
 ByteArrayQueue baq = new ByteArrayQueue();
@@ -97,20 +104,19 @@ try (FileOutputStream file2 = new FileOutputStream(...)) {
 
 ### Service
 
-### Base64 (JDK 1.7 Only)
-
+### Base64
 Encode Byte Array to Base64 String or Decode Base64 String to Byte Array.
 
-Use [`java.util.Base64`](http://docs.oracle.com/javase/8/docs/api/java/util/Base64.html) for JDK 1.8.
+Use [**java.util.Base64**](http://docs.oracle.com/javase/8/docs/api/java/util/Base64.html) for JDK 1.8.
 
-### HTTP Utils
+### Database Connections
 
-### DB Utils
+### HTTP Client Operations
 
 ### Servlet Utils
 
-### JavaScript Utils
+### JavaScript Server Page
 
-### Concurrent Utils
+### CountMap and LockMap
 
-### Lambda Utils
+### Functional Interfaces
--- util/src/main/java/com/xqbase/util/Bytes.java
+++ util/src/main/java/com/xqbase/util/Bytes.java
@@ -14,9 +14,12 @@ import java.util.Random;
  * <li>Dumping for debug</li>
  */
 public class Bytes {
+	/** Not Little-Ending, i.e. storing/retrieving numbers in Big-Ending (default) byte order */
 	public static final boolean BIG_ENDIAN = false;
+	/** Little-Ending, i.e. storing/retrieving numbers in Little-Ending byte order */
 	public static final boolean LITTLE_ENDIAN = true;
 
+	/** A byte array with length of <b>0</b> */
 	public static final byte[] EMPTY_BYTES = {};
 
 	private static final char[] HEX_UPPER_CHAR = "0123456789ABCDEF".toCharArray();
@@ -41,11 +44,13 @@ public class Bytes {
 		0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 	};
 
+	/** Decode from hexadecimal */
 	public static byte[] fromHex(String hex) {
 		char[] c = hex.toCharArray();
 		byte[] b = new byte[c.length / 2];
 		for (int i = 0; i < b.length; i ++) {
-			b[i] = (byte) (HEX_DECODE_CHAR[c[i * 2] & 0xFF] * 16 + HEX_DECODE_CHAR[c[i * 2 + 1] & 0xFF]);
+			b[i] = (byte) (HEX_DECODE_CHAR[c[i * 2] & 0xFF] * 16 +
+					HEX_DECODE_CHAR[c[i * 2 + 1] & 0xFF]);
 		}
 		return b;
 	}
@@ -60,38 +65,46 @@ public class Bytes {
 		return new String(c);
 	}
 
+	/** Encode to uppercase hexadecimal */
 	public static String toHexUpper(byte[] b) {
 		return toHexUpper(b, 0, b.length);
 	}
 
+	/** Encode to uppercase hexadecimal */
 	public static String toHexUpper(byte[] b, int off, int len) {
 		return toHex(b, off, len, HEX_UPPER_CHAR);
 	}
 
+	/** Encode to lowercase hexadecimal */
 	public static String toHexLower(byte[] b) {
 		return toHexLower(b, 0, b.length);
 	}
 
+	/** Encode to lowercase hexadecimal */
 	public static String toHexLower(byte[] b, int off, int len) {
 		return toHex(b, off, len, HEX_LOWER_CHAR);
 	}
 
+	/** Generate a 2-byte array from a <b>short</b> number in Big-Endian byte order */
 	public static byte[] fromShort(int n) {
 		return fromShort(n, BIG_ENDIAN);
 	}
 
-	public static byte[] fromShort(int n, boolean bLittleEndian) {
+	/** Generate a 2-byte array from a <b>short</b> number in a given byte order */
+	public static byte[] fromShort(int n, boolean littleEndian) {
 		byte[] b = new byte[2];
-		setShort(n, b, 0, bLittleEndian);
+		setShort(n, b, 0, littleEndian);
 		return b;
 	}
 
+	/** Store a <b>short</b> number into a byte array in Big-Endian byte order */
 	public static void setShort(int n, byte[] b, int off) {
 		setShort(n, b, off, BIG_ENDIAN);
 	}
 
-	public static void setShort(int n, byte[] b, int off, boolean bLittleEndian) {
-		if (bLittleEndian) {
+	/** Store a <b>short</b> number into a byte array in a given byte order */
+	public static void setShort(int n, byte[] b, int off, boolean littleEndian) {
+		if (littleEndian) {
 			b[off] = (byte) n;
 			b[off + 1] = (byte) (n >>> 8);
 		} else {
@@ -100,22 +113,26 @@ public class Bytes {
 		}
 	}
 
+	/** Generate a 4-byte array from an <b>int</b> number in Big-Endian byte order */
 	public static byte[] fromInt(int n) {
 		return fromInt(n, BIG_ENDIAN);
 	}
 
-	public static byte[] fromInt(int n, boolean bLittleEndian) {
+	/** Generate a 4-byte array from an <b>int</b> number in a given byte order */
+	public static byte[] fromInt(int n, boolean littleEndian) {
 		byte[] b = new byte[4];
-		setInt(n, b, 0, bLittleEndian);
+		setInt(n, b, 0, littleEndian);
 		return b;
 	}
 
+	/** Store an <b>int</b> number into a byte array in Big-Endian byte order */
 	public static void setInt(int n, byte[] b, int off) {
 		setInt(n, b, off, BIG_ENDIAN);
 	}
 
-	public static void setInt(int n, byte[] b, int off, boolean bLittleEndian) {
-		if (bLittleEndian) {
+	/** Store an <b>int</b> number into a byte array in a given byte order */
+	public static void setInt(int n, byte[] b, int off, boolean littleEndian) {
+		if (littleEndian) {
 			b[off] = (byte) n;
 			b[off + 1] = (byte) (n >>> 8);
 			b[off + 2] = (byte) (n >>> 16);
@@ -128,22 +145,26 @@ public class Bytes {
 		}
 	}
 
+	/** Generate an 8-byte array from a <b>long</b> number in Big-Endian byte order */
 	public static byte[] fromLong(long n) {
 		return fromLong(n, BIG_ENDIAN);
 	}
 
-	public static byte[] fromLong(long n, boolean bLittleEndian) {
+	/** Generate an 8-byte array from a <b>long</b> number in a given byte order */
+	public static byte[] fromLong(long n, boolean littleEndian) {
 		byte[] b = new byte[8];
-		setLong(n, b, 0, bLittleEndian);
+		setLong(n, b, 0, littleEndian);
 		return b;
 	}
 
+	/** Store a <b>long</b> number into a byte array in Big-Endian byte order */
 	public static void setLong(long n, byte[] b, int off) {
 		setLong(n, b, off, BIG_ENDIAN);
 	}
 
-	public static void setLong(long n, byte[] b, int off, boolean bLittleEndian) {
-		if (bLittleEndian) {
+	/** Store a <b>long</b> number into a byte array in a given byte order */
+	public static void setLong(long n, byte[] b, int off, boolean littleEndian) {
+		if (littleEndian) {
 			setInt((int) n, b, off, LITTLE_ENDIAN);
 			setInt((int) (n >>> 32), b, off + 4, LITTLE_ENDIAN);
 		} else {
@@ -152,63 +173,80 @@ public class Bytes {
 		}
 	}
 
+	/** Retrieve a <b>short</b> from a byte array in Big-Endian byte order */
 	public static int toShort(byte[] b) {
 		return toShort(b, 0);
 	}
 
-	public static int toShort(byte[] b, boolean bLittleEndian) {
-		return toShort(b, 0, bLittleEndian);
+	/** Retrieve a <b>short</b> from a byte array in a given byte order */
+	public static int toShort(byte[] b, boolean littleEndian) {
+		return toShort(b, 0, littleEndian);
 	}
 
+	/** Retrieve a <b>short</b> from a byte array in Big-Endian byte order */
 	public static int toShort(byte[] b, int off) {
 		return toShort(b, off, BIG_ENDIAN);
 	}
 
-	public static int toShort(byte[] b, int off, boolean bLittleEndian) {
-		if (bLittleEndian) {
+	/** Retrieve a <b>short</b> from a byte array in a given byte order */
+	public static int toShort(byte[] b, int off, boolean littleEndian) {
+		if (littleEndian) {
 			return ((b[off] & 0xFF) | ((b[off + 1] & 0xFF) << 8));
 		}
 		return (((b[off] & 0xFF) << 8) | (b[off + 1] & 0xFF));
 	}
 
+	/** Retrieve an <b>int</b> from a byte array in Big-Endian byte order */
 	public static int toInt(byte[] b) {
 		return toInt(b, 0);
 	}
 
-	public static int toInt(byte[] b, boolean bLittleEndian) {
-		return toInt(b, 0, bLittleEndian);
+	/** Retrieve an <b>int</b> from a byte array in a given byte order */
+	public static int toInt(byte[] b, boolean littleEndian) {
+		return toInt(b, 0, littleEndian);
 	}
 
+	/** Retrieve an <b>int</b> from a byte array in Big-Endian byte order */
 	public static int toInt(byte[] b, int off) {
 		return toInt(b, off, BIG_ENDIAN);
 	}
 
-	public static int toInt(byte[] b, int off, boolean bLittleEndian) {
-		if (bLittleEndian) {
-			return (b[off] & 0xFF) | ((b[off + 1] & 0xFF) << 8) | ((b[off + 2] & 0xFF) << 16) | ((b[off + 3] & 0xFF) << 24);
+	/** Retrieve an <b>int</b> from a byte array in a given byte order */
+	public static int toInt(byte[] b, int off, boolean littleEndian) {
+		if (littleEndian) {
+			return (b[off] & 0xFF) | ((b[off + 1] & 0xFF) << 8) |
+					((b[off + 2] & 0xFF) << 16) | ((b[off + 3] & 0xFF) << 24);
 		}
-		return ((b[off] & 0xFF) << 24) | ((b[off + 1] & 0xFF) << 16) | ((b[off + 2] & 0xFF) << 8) | (b[off + 3] & 0xFF);
+		return ((b[off] & 0xFF) << 24) | ((b[off + 1] & 0xFF) << 16) |
+				((b[off + 2] & 0xFF) << 8) | (b[off + 3] & 0xFF);
 	}
 
+	/** Retrieve a <b>long</b> from a byte array in Big-Endian byte order */
 	public static long toLong(byte[] b) {
 		return toLong(b, 0);
 	}
 
-	public static long toLong(byte[] b, boolean bLittleEndian) {
-		return toLong(b, 0, bLittleEndian);
+	/** Retrieve a <b>long</b> from a byte array in a given byte order */
+	public static long toLong(byte[] b, boolean littleEndian) {
+		return toLong(b, 0, littleEndian);
 	}
 
+	/** Retrieve a <b>long</b> from a byte array in Big-Endian byte order */
 	public static long toLong(byte[] b, int off) {
 		return toLong(b, off, BIG_ENDIAN);
 	}
 
-	public static long toLong(byte[] b, int off, boolean bLittleEndian) {
-		if (bLittleEndian) {
-			return (toInt(b, off, LITTLE_ENDIAN) & 0xFFFFFFFFL) | ((toInt(b, off + 4, LITTLE_ENDIAN) & 0xFFFFFFFFL) << 32);
+	/** Retrieve a <b>long</b> from a byte array in a given byte order */
+	public static long toLong(byte[] b, int off, boolean littleEndian) {
+		if (littleEndian) {
+			return (toInt(b, off, LITTLE_ENDIAN) & 0xFFFFFFFFL) |
+					((toInt(b, off + 4, LITTLE_ENDIAN) & 0xFFFFFFFFL) << 32);
 		}
-		return ((toInt(b, off, BIG_ENDIAN) & 0xFFFFFFFFL) << 32) | (toInt(b, off + 4, BIG_ENDIAN) & 0xFFFFFFFFL);
+		return ((toInt(b, off, BIG_ENDIAN) & 0xFFFFFFFFL) << 32) |
+				(toInt(b, off + 4, BIG_ENDIAN) & 0xFFFFFFFFL);
 	}
 
+	/** Concatenate 2 byte arrays */
 	public static byte[] add(byte[] b1, int off1, int len1, byte[] b2, int off2, int len2) {
 		byte[] b = new byte[len1 + len2];
 		System.arraycopy(b1, off1, b, 0, len1);
@@ -216,6 +254,7 @@ public class Bytes {
 		return b;
 	}
 
+	/** Concatenate <i>n</i> byte arrays */
 	public static byte[] add(byte[]... b) {
 		int nLen = 0;
 		for (int i = 0; i < b.length; i ++) {
@@ -231,28 +270,34 @@ public class Bytes {
 		return lp;
 	}
 
+	/** Clone a byte array */
 	public static byte[] clone(byte[] b) {
 		return sub(b, 0, b.length);
 	}
 
+	/** Truncate and keep left part of a byte array */
 	public static byte[] left(byte[] b, int len) {
 		return sub(b, 0, len);
 	}
 
+	/** Truncate and keep right part of a byte array (by a given length) */
 	public static byte[] right(byte[] b, int len) {
 		return sub(b, b.length - len, len);
 	}
 
+	/** Truncate and keep right part of a byte array (by a given position) */
 	public static byte[] sub(byte[] b, int off) {
 		return sub(b, off, b.length - off);
 	}
 
+	/** Truncate and keep middle part of a byte array */
 	public static byte[] sub(byte[] b, int off, int len) {
 		byte[] result = new byte[len];
 		System.arraycopy(b, off, result, 0, len);
 		return result;
 	}
 
+	/** Check whether a byte array equals to another */
 	public static boolean equals(byte[] b1, int off1, byte[] b2, int off2, int len) {
 		if (b1 == b2 && off1 == off2) {
 			return true;
@@ -265,6 +310,7 @@ public class Bytes {
 		return true;
 	}
 
+	/** Check whether a byte array equals to another */
 	public static boolean equals(byte[] b1, byte[] b2) {
 		return b1.length == b2.length && equals(b1, 0, b2, 0, b1.length);
 	}
@@ -278,44 +324,47 @@ public class Bytes {
 		return result;
 	}
 
+	/** Generate a random byte array by a given length */
 	public static byte[] random(int len) {
 		return random(len, random);
 	}
 
+	/** Generate a random byte array by a given length, using secure random number generator */
 	public static byte[] secureRandom(int len) {
 		return random(len, secureRandom);
 	}
 
 	private static String dumpLine(byte[] b, int offDiv16, int begin, int end) {
-		char[] lpLine = "                                                                             ".toCharArray();
-		//				"0000:0000  00 00 00 00 00 00 00 00-00 00 00 00 00 00 00 00   ................"
-		lpLine[0] = HEX_UPPER_CHAR[(offDiv16 >>> 24) & 0xF];
-		lpLine[1] = HEX_UPPER_CHAR[(offDiv16 >>> 20) & 0xF];
-		lpLine[2] = HEX_UPPER_CHAR[(offDiv16 >>> 16) & 0xF];
-		lpLine[3] = HEX_UPPER_CHAR[(offDiv16 >>> 12) & 0xF];
-		lpLine[4] = ':';
-		lpLine[5] = HEX_UPPER_CHAR[(offDiv16 >>> 8) & 0xF];
-		lpLine[6] = HEX_UPPER_CHAR[(offDiv16 >>> 4) & 0xF];
-		lpLine[7] = HEX_UPPER_CHAR[offDiv16 & 0xF];
-		lpLine[8] = '0';
+		char[] line = "                                                                             ".toCharArray();
+		//			  "0000:0000  00 00 00 00 00 00 00 00-00 00 00 00 00 00 00 00   ................"
+		line[0] = HEX_UPPER_CHAR[(offDiv16 >>> 24) & 0xF];
+		line[1] = HEX_UPPER_CHAR[(offDiv16 >>> 20) & 0xF];
+		line[2] = HEX_UPPER_CHAR[(offDiv16 >>> 16) & 0xF];
+		line[3] = HEX_UPPER_CHAR[(offDiv16 >>> 12) & 0xF];
+		line[4] = ':';
+		line[5] = HEX_UPPER_CHAR[(offDiv16 >>> 8) & 0xF];
+		line[6] = HEX_UPPER_CHAR[(offDiv16 >>> 4) & 0xF];
+		line[7] = HEX_UPPER_CHAR[offDiv16 & 0xF];
+		line[8] = '0';
 		for (int i = 0; i < 16; i ++) {
 			if (i >= begin && i < end) {
-				int bInt = b[offDiv16 * 16 + i];
-				lpLine[i * 3 + 11] = HEX_UPPER_CHAR[(bInt & 0xF0) >> 4];
-				lpLine[i * 3 + 12] = HEX_UPPER_CHAR[bInt & 0xF];
-				if (bInt >= 32 && bInt < 127) {
-					lpLine[61 + i] = (char) bInt;
+				int b_ = b[offDiv16 * 16 + i];
+				line[i * 3 + 11] = HEX_UPPER_CHAR[(b_ & 0xF0) >> 4];
+				line[i * 3 + 12] = HEX_UPPER_CHAR[b_ & 0xF];
+				if (b_ >= 32 && b_ < 127) {
+					line[61 + i] = (char) b_;
 				} else {
-					lpLine[61 + i] = '.';
+					line[61 + i] = '.';
 				}
 			}
 		}
 		if (begin < 8 && end > 8) {
-			lpLine[34] = '-';
+			line[34] = '-';
 		}
-		return new String(lpLine);
+		return new String(line);
 	}
 
+	/** Dump a byte array into a {@link PrintStream} in human readable format */
 	public static void dump(PrintStream out, byte[] b, int off, int len) {
 		int end = off + len;
 		int offDiv16 = off / 16;
@@ -333,10 +382,12 @@ public class Bytes {
 		}
 	}
 
+	/** Dump a byte array into a {@link PrintStream} in human readable format */
 	public static void dump(PrintStream out, byte[] b) {
 		dump(out, b, 0, b.length);
 	}
 
+	/** Dump a byte array into a {@link PrintWriter} in human readable format */
 	public static void dump(PrintWriter out, byte[] b, int off, int len) {
 		int end = off + len;
 		int offDiv16 = off / 16;
@@ -354,6 +405,7 @@ public class Bytes {
 		}
 	}
 
+	/** Dump a byte array into a {@link PrintWriter} in human readable format */
 	public static void dump(PrintWriter out, byte[] b) {
 		dump(out, b, 0, b.length);
 	}
