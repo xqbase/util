46d52df5dd74e965d6639d24d5dd7a53f7da4014
auntyellow <webmaster@xqbase.com>
2017-01-24 10:37:14 +0800

    y-combinator: more test

--- util/src/main/java/com/xqbase/util/function/Recursive.java
+++ util/src/main/java/com/xqbase/util/function/Recursive.java
@@ -4,15 +4,17 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 @FunctionalInterface
-interface Generator<T> extends Function<Generator<T>, Function<T, T>> {/**/}
-
-@FunctionalInterface
 public interface Recursive<T> extends BiFunction<Recursive<T>, T, T> {
+	@FunctionalInterface
+	public static interface Generator<T>
+			extends Function<Generator<T>, Function<T, T>> {/**/}
+
 	public static <T> Function<T, T> wrap(Recursive<T> func) {
 		return t -> func.apply(func, t);
 	}
 
-	public static <T> Function<T, T> generate(BiFunction<Function<T, T>, T, T> func) {
+	public static <T> Function<T, T>
+			generate(BiFunction<Function<T, T>, T, T> func) {
 		Generator<T> gen = self -> t -> func.apply(self.apply(self), t);
 		return gen.apply(gen);
 	}
--- util/src/test/java/com/xqbase/util/function/TestRecursive.java
+++ util/src/test/java/com/xqbase/util/function/TestRecursive.java
@@ -3,20 +3,33 @@ package com.xqbase.util.function;
 import java.util.function.Function;
 
 public class TestRecursive {
+	private static int fibonacci(int n) {
+		return n <= 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+	private static final int TEST_INPUT = 45;
+
 	@SuppressWarnings("boxing")
 	public static void main(String[] args) {
+		long t0 = System.currentTimeMillis();
+
+		int result = fibonacci(TEST_INPUT);
+		long t1 = System.currentTimeMillis();
+		System.out.println("Fibonacci(" + TEST_INPUT + ")=" + result +
+				", with Recursive: " + (t1 - t0) + "ms");
+
 		Function<Integer, Integer> fibonacci = Recursive.wrap((self, n) ->
 				n <= 1 ? 1 : self.apply(self, n - 1) + self.apply(self, n - 2));
-		for (int i = 0; i < 16; i ++) {
-			System.out.print(fibonacci.apply(i) + " ");
-		}
-		System.out.println();
+		result = fibonacci.apply(TEST_INPUT);
+		long t2 = System.currentTimeMillis();
+		System.out.println("Fibonacci(" + TEST_INPUT + ")=" + result +
+				", with Recursive Lambda: " + (t2 - t1) + "ms");
 
 		fibonacci = Recursive.generate((self, n) ->
 				n <= 1 ? 1 : self.apply(n - 1) + self.apply(n - 2));
-		for (int i = 0; i < 16; i ++) {
-			System.out.print(fibonacci.apply(i) + " ");
-		}
-		System.out.println();
+		result = fibonacci.apply(TEST_INPUT);
+		long t3 = System.currentTimeMillis();
+		System.out.println("Fibonacci(" + TEST_INPUT + ")=" + result +
+				", with Recursive Lambda and Y-Combinator: " + (t3 - t2) + "ms");
 	}
 }
