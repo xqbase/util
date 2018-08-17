6429270a631a5b8cf5a3b1b6e2e9d67483cb8c83
auntyellow <webmaster@xqbase.com>
2018-08-17 23:11:34 +0800

    add ip138

---
+++ util-sns/src/main/java/com/xqbase/util/sns/Ip138Ip.java
@@ -0,0 +1,51 @@
+package com.xqbase.util.sns;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.xqbase.util.ByteArrayQueue;
+import com.xqbase.util.http.HttpPool;
+
+public class Ip138Ip {
+	public static class Exception extends java.lang.Exception {
+		private static final long serialVersionUID = 1L;
+
+		public Exception(String message) {
+			super(message);
+		}
+	}
+
+	private static final String SERVICE_URL =
+			"http://m.ip138.com/ip.asp?ip=";
+
+	private static HttpPool httpPool = new HttpPool(SERVICE_URL, 15000);
+
+	public static HttpPool getHttpPool() {
+		return httpPool;
+	}
+
+	public static String getIpInfo(String ip) throws Exception {
+		ByteArrayQueue response = new ByteArrayQueue();
+		try {
+			int status = httpPool.get(ip, null, response, null);
+			if (status != 200) {
+				throw new Exception("Error Getting " + ip +
+						": " + status + ", " + response);
+			}
+			String html = response.toString(StandardCharsets.UTF_8);
+			int i = html.indexOf("<p class=\"result\">本站主数据：");
+			if (i < 0) {
+				throw new Exception("Error Getting " + ip + ": " + html);
+			}
+			i += 24;
+			int j = html.indexOf("</p>", i);
+			if (j < 0) {
+				throw new Exception("Error Getting " + ip + ": " + html);
+			}
+			return html.substring(i, j);
+		} catch (IOException e) {
+			throw new Exception("Error Getting " + ip +
+					": " + e.getMessage() + ", " + response);
+		}
+	}
+}
